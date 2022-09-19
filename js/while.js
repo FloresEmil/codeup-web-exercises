56fcd164ce2184d2816d1c84126b6a5800745361
Emil Flores <emil.alejandro.flores@gmail.com>
2022-09-19 11:56:32 -0500

    for loops exercises completed

--- js/while.js
+++ js/while.js
@@ -1,4 +1,27 @@
 "use strict"
+//Create a file named while.js in the js directory.
+//
+//	Create a while loop that uses console.log() to create the output shown below.
+//
+//
+//2
+//4
+//8
+//16
+//32
+//64
+//128
+//256
+//512
+//1024
+//2048
+//4096
+//8192
+//16384
+//32768
+//65536
+
+
 let num = 2;
 while (num <= 65536){
 	console.log(num);
