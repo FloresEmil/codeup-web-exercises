11b865b34c22c72fb019714915600ad12050d936
Emil Flores <emil.alejandro.flores@gmail.com>
2022-10-03 09:23:58 -0500

    warm up for oct 3rd done

--- js/warmup_exercises.js
+++ js/warmup_exercises.js
@@ -1,28 +1,14 @@
 "use strict"
 
-//Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
-// example input: [1,2,3,4,5] expected output: [2,4]
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-function isEven(arr) {
-	let count = 0;
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 === 0) {
-			count++;
-		}
-	}
-	return count;
-}
-
-console.log(isEven())
+//re-write the following using a for loop: var i = 20; while (i >= 0) { console.log(i); i--; }
+for (let i = 20; 1>= 0; i--)
+	console.log(i)
 
 
+//Add ‘strawberry’ to the beginning, middle, and end of the array.
+let fruits = ["mango", "blueberries", "oranges", "banana", "papaya", "kiwi"];
 
-
-
-// Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
-// example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
-// expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
-
-
+fruits.unshift("strawberry")
+fruits.push("strawberry")
+fruits.splice(4, 0, "strawberry")
+console.log(fruits)
