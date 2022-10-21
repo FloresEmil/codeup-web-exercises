09b01154a2a0b13e6def700f87fe9e3787044d7e
Emil Flores <emil.alejandro.flores@gmail.com>
2022-10-21 10:53:26 -0500

    jquery exercises done

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,9 +1,38 @@
 "use strict";
+//
+// // $("#1").click(function () {
+// // 	alert("hola")
+// // });
+// //
+// // // $(function () {
+// // // 	alert('The DOM has finished loading!');
+// // // });
+// //
+// //
+// // $(".codeup").css("border", "solid 1px red")
+//
+//
+// $("li").css("font-size", "20px")
+//
+// $(".things").css("background-color", "red")
+//
+//
+// $("h1").click(function () {
+// 	alert("heeeeey thats pretty good")
+// })
 
-$('p').click(function () {
-	$(this).css('background-color', '#d685de');
-});
 
-$(function () {
-	alert('The DOM has finished loading!');
-});
+$("h1").click(function () {
+	$("h1").css("background-color", "pink");
+})
+
+$("p").dblclick(function () {
+	$("p").css("font-size", "18px");
+})
+
+$("li").hover(function () {
+	$("li").css("color", "red");
+}, function () {
+	$(li).css("color: '.$status_color.'")
+
+})
